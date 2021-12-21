871016b1b1fb6a5620247bab3276442fa8efeab9
BuildTools <unconfigured@null.spigotmc.org>
2021-12-21 01:40:50 -0300

    chebo

---
+++ commands/admin/chebo.js
@@ -0,0 +1,30 @@
+const { Permissions } = require("discord.js");
+const mysql = require("mysql");
+const conn = mysql.createConnection({
+  host            : `localhost`,
+  user            : `${process.env.DB_USER}`,
+  password        : `${process.env.DB_PASS}`,
+  database        : 'sugerencias'
+});
+conn.connect(function(err){
+  if (err){
+    console.log(err)
+  }else{
+    console.log(`- Conectado a 'sugerencias' -`);
+  }
+}); 
+module.exports = {
+  name: 'chebo',
+  aliases: [],
+  category: "admin",
+  description: "Envia una sugerencia, error, o arreglo hacia los creadores de este humilde bot",
+  usage: ">chebo [sugerencia, error, arreglo]",
+  async execute(client, message, args) {
+    if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return;
+    let contenido = args.slice(1).join(" ");
+    conn.query(`INSERT INTO sugerencia (dname, sugerencia) VALUES ('${message.member.user.tag}','${contenido}')`,(message, err) => {
+    if (err) console.log(err); 
+    });
+    message.author.send(`${message.member.user.tag}: Tu mensaje se envio correctamente, gracias por tu ayuda`);
+  },
+};
--- commands/admin/join.js
+++ commands/admin/join.js
@@ -1,14 +1,14 @@
-const {Permissions} = require('discord.js')
+const { Permissions } = require("discord.js");
 module.exports = {
-name: "join",
-aliases: [],
-category: "adminHades",
-description: "Command description",
-usage: "[args input]",
-async execute (client, message, args) {
-    if(!message.guild.id === '891140067282804767')return;
+  name: "join",
+  aliases: [],
+  category: "adminHades",
+  description: "Command description",
+  usage: "[args input]",
+  async execute(client, message, args) {
+    if (!message.guild.id === "891140067282804767") return;
     message.delete();
-    if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return
-    client.emit('guildMemberAdd', message.member);
-}
-}
+    if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return;
+    client.emit("guildMemberAdd", message.member);
+  },
+};
