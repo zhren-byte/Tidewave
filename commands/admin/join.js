c052001cc70a29687e4f2de2e39397f836d41560
BuildTools <unconfigured@null.spigotmc.org>
2022-03-14 22:38:53 -0300

    test

--- commands/admin/join.js
+++ commands/admin/join.js
@@ -6,7 +6,6 @@ module.exports = {
   description: "Command description",
   usage: "[args input]",
   async execute(client, message, args) {
-    if (!message.guild.id === "891140067282804767") return;
     message.delete();
     if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return;
     client.emit("guildMemberAdd", message.member);
