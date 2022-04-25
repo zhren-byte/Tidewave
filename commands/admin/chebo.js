9599c7b09c891163930a71233b2e114bbad47e1d
BuildTools <unconfigured@null.spigotmc.org>
2022-04-24 23:39:59 -0300

    update 24/4

--- commands/admin/chebo.js
+++ commands/admin/chebo.js
@@ -1,10 +1,14 @@
 const { Permissions } = require("discord.js");
 const mysql = require("mysql");
 const conn = mysql.createConnection({
-  host: `${process.env.DB_HOST}`,
-  port: `${process.env.DB_PORT}`,
-  user: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASS}`,
+  // host: `${process.env.DB_HOST}`,
+  // port: `${process.env.DB_PORT}`,
+  // user: `${process.env.DB_USER}`,
+  // password: `${process.env.DB_PASS}`,
+  // database: "sugerencias",
+  host: `localhost`,
+  user: `zhren`,
+  password: `777zhren777`,
   database: "sugerencias",
 });
 module.exports = {
@@ -15,39 +19,39 @@ module.exports = {
   usage: ">chebo [sugerencia, error, arreglo]",
   async execute(client, message, args) {
     if (!message.member.permissions.has(Permissions.FLAGS.MANAGE_GUILD)) return;
-    conn.connect(function (err) {
-      if (err) {
-        console.log("Base de datos no disponible");
-        console.log(err);
-      } else {
-        console.log(
-          `- Se envio una nueva propuesta desde -> ${message.member.user.tag}`
-        );
-      }
-    });
-    let contenido = args.slice(0).join(" ");
+    let contenido = args
+      .slice(0)
+      .join(" ")
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&apos;");
     let date = message.createdAt
       .toISOString()
       .replace(/T/, " ")
       .replace(/\..+/, "");
-    conn.query(`SELECT count(*) AS count FROM sugerencia`,
-    (err, results) => {
+    conn.query(`SELECT count(*) AS count FROM sugerencia`, (err, results) => {
       if (err) console.log(err);
       let id = results[0].count;
-      quest = conn.query(
-        `INSERT INTO sugerencia (dname, sugerencia, fecha) VALUES ('${message.member.user.tag}','${contenido}', '${date}')`,
-        (message, err) => {
+      conn.query(
+        `INSERT INTO sugerencia (id, dname, sugerencia, fecha) VALUES ('${
+          id + 1
+        }', '${message.member.user.tag}','${contenido}', '${date}')`,
+        (third, err) => {
           if (err) {
             console.log(err);
             return;
+          } else {
+            console.log(
+              `- Se envio una nueva propuesta desde -> ${message.member.user.tag}`
+            );
           }
-          
         }
       );
     });
-    message.author.send(
-      `${message.member.user.tag}: Tu mensaje se envio correctamente, gracias por tu ayuda`
-    );
-    conn.end();
+    // message.author.send(
+    //   `${message.member.user.tag}: Tu mensaje se envio correctamente, gracias por tu ayuda`
+    // );
   },
 };
