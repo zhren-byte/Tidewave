800c2cd0b0235e90fbb0c46811c9fc0b879620cf
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 13:02:30 -0300

    Clean

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  _id: Number,
-  userName: String,
-  warns: [
-    {
-      _id: Number,
-      warn: Number,
-    },
-  ],
+	_id: Number,
+	userName: String,
+	warns: [
+		{
+			_id: Number,
+			warn: Number,
+		},
+	],
 });
 
-module.exports = mongoose.model("User", userSchema, "user");
+module.exports = mongoose.model('User', userSchema, 'user');
