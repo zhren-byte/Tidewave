e3d29a6df72b3b5991e98588d96d3ac9eb135e95
BuildTools <unconfigured@null.spigotmc.org>
2022-05-15 13:25:28 -0300

    Warn

--- index.js
+++ index.js
@@ -148,7 +148,6 @@ client.on('messageCreate', async (message) => {
 	const userSet = await User.findOne(
 		{
 			_id: message.author.id,
-			'warns._id': message.guild.id,
 		},
 		(err, user) => {
 			if (err) console.error(err);
@@ -156,12 +155,6 @@ client.on('messageCreate', async (message) => {
 				const newUser = new User({
 					_id: message.author.id,
 					userName: message.author.username,
-					warns: [
-						{
-							_id: message.guild.id,
-							warn: 0,
-						},
-					],
 				});
 				newUser.save().catch((err) => console.error(err));
 			}
