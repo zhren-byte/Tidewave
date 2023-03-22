09c0b87d08f72f7d71a2dec992b9273592a2e128
Thomas <38599635+zhren-byte@users.noreply.github.com>
2023-03-22 13:02:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(session({
 	key: 'session_cookie_name',
 	secret: 'session_cookie_secret',
 	store: new MySQLStore({
-	host: process.env.DB_HOST,
-  	port: process.env.DB_PORT,
-  	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
+	host: `${process.env.DB_HOST}`,
+  	port: `${process.env.DB_PORT}`,
+  	user: `${process.env.DB_USER}`,
+	password: `${process.env.DB_PASS}`,
 	database:'cookie_user',
 	}),
 	resave: false,
@@ -34,10 +34,10 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 const conn = mysql.createConnection({
-	host: process.env.DB_HOST,
-  	port: process.env.DB_PORT,
-  	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
+	host: `${process.env.DB_HOST}`,
+  	port: `${process.env.DB_PORT}`,
+  	user: `${process.env.DB_USER}`,
+	password: `${process.env.DB_PASS}`,
 	database: 'tidewave',
 	multipleStatements: true,
 });
